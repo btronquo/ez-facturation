a53a028ad8cb65cde841cc22227edc2c66ae716a
Boris Tronquoy <b.tronquoy@gmail.com>
2020-01-22 13:48:45 +0100

    :lock: (change auth type): add laravel passport

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::apiResources(['customer' => 'API\CustomerController']);
+
+Route::group(['middleware' => 'auth:api'], function() {
+Route::apiResources([
+      'customer' => 'API\CustomerController'
+  ]);
+});
+
